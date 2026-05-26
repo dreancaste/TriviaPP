64133873e9fa09c9830cf6698363afb9b53c8b2b
dreancaste <luciano_l17@hotmail.com>
2026-05-26 19:20:48 -0300

    commit test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,8 @@ const routes: Routes = [
   {
     path: 'home',
     canActivate: [AuthGuard],
-    loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule),
+    
   },
   {
     path: 'profile',
