ff54b1dce2648faa5d41ab60138d5635d2234f01
Axel Contreras <axlcontreras96@gmail.com>
2026-06-02 00:01:36 -0300

    Se ajusta login cuando ya esta guardado y boton atras estando en home no desloguea

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -15,8 +15,24 @@ export class LoginPage {
 
   constructor(
     private authService: AuthService,
-    private router: Router
+    private navCtrl: NavController
   ) {}
+
+  async ionViewWillEnter() {
+    try {
+      //chequeamos usuario activo
+      const usuarioActivo = await this.authService.getCurrentUser();
+      
+      //nos dirigimos con funcion de ionic al home directo
+      if (usuarioActivo) {
+        this.navCtrl.navigateRoot('/home');
+      }
+    } catch (error) {
+      //si hay error vamos a login ---CHQUEAR SACAR EL CONSOLE LOG---
+      console.log('No hay sesión activa, listo para loguear.');
+    }
+  }
+
   
   // Login, requiere email y contrasenia correcta.
 
@@ -31,7 +47,7 @@ export class LoginPage {
     try {
       this.loading = true;
       await this.authService.login(this.email, this.password);
-      this.router.navigateByUrl('/home', { replaceUrl: true });
+      this.navCtrl.navigateRoot('/home', { replaceUrl: true });
     } catch (error) {
       this.errorMessage = 'No se pudo iniciar sesión';
       console.error(error);
@@ -43,6 +59,6 @@ export class LoginPage {
   // Redirije a la pagina registro.
 
   goToRegister() {
-    this.router.navigateByUrl('/register');
+    this.navCtrl.navigateForward('/register');
   }
 }
